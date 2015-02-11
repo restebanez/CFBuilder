d5ac0c4709bf4e3cdf54d72b882e0f1367056a9d
Rodrigo Estebanez <restebanez@mdsol.com>
2015-02-11 16:39:33 +0000

    show nicer errors

--- js/components/security_group/input_with_msg.js
+++ js/components/security_group/input_with_msg.js
@@ -5,6 +5,11 @@ var Formsy = require('formsy-react');
 
 
 var InputWithMsg = React.createClass({
+    getInitialState: function(){
+        return {
+            focus: false
+        }
+    },
 
     // Add the Formsy Mixin
     mixins: [Formsy.Mixin],
@@ -14,6 +19,13 @@ var InputWithMsg = React.createClass({
     changeValue: function (event) {
         this.setValue(event.currentTarget.value);
     },
+
+    setFocus: function(){
+        this.setState({focus: true})
+    },
+    setBlur: function(){
+        this.setState({focus: false})
+    },
     render: function () {
 
         // Set a specific className based on the validation
@@ -21,16 +33,18 @@ var InputWithMsg = React.createClass({
         // when the value is empty and the required prop is
         // passed to the input. showError() is true when the
         // value typed is invalid
-        var className = this.showRequired() ? 'required' : this.showError() ? 'has-error ' : null;
+        var className = this.showRequired() ? 'has-warning' : this.showError() ? 'has-error ' : 'has-success';
 
         // An error message is returned ONLY if the component is invalid
         // or the server has returned an error message
         var errorMessage = this.getErrorMessage();
+        var glyphiconIconClass = this.showRequired() ? 'glyphicon-warning-sign' : this.showError() ? 'glyphicon-remove ' : 'glyphicon-ok';
+        var iconValidation = this.state.focus ? <span className={'form-control-feedback glyphicon ' + glyphiconIconClass} aria-hidden="true"></span> : '';
         return (
 
-            <div className={'form-group has-feedback ' + className} >
-                <input className="form-control" type="text" onChange={this.changeValue} value={this.getValue()} aria-describedby="inputError2Status"/>
-                <span className="glyphicon glyphicon-remove form-control-feedback" aria-hidden="true"></span>
+            <div className={'form-group ' + className + ' has-feedback' } >
+                <input className="form-control" onFocus={this.setFocus} onBlur={this.setBlur} type="text" onChange={this.changeValue} value={this.getValue()} aria-describedby="inputError2Status"/>
+               {iconValidation}
                 <span id="inputError2Status">{errorMessage}</span>
             </div>
 
